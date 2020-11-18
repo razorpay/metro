e52521f3f01c6e52b79514be3a45e4635eb7022c
Shivasish Das <shivasish.das@razorpay.com>
2020-11-18 17:10:55 +0530

    minor changes

--- build/docker/probe.sh
+++ build/docker/probe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #validating the status code of health check api and returning 1 in case of non 2xx status code
-if [ "$(curl -s -o /dev/null -H 'Content-Type: application/json' -d '{"service": ""}' -w '%{http_code}' http://localhost:8080/v1/healthcheck)" != 200 ];
+if [ "$(curl -s -o /dev/null -H 'Content-Type: application/json' -d '{"service": ""}' -w '%{http_code}' http://localhost:8082/v1/healthcheck)" != 200 ];
 then
   exit 1;
 fi

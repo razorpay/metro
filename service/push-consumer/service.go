3e2cbd27805bd7b1e5c5feb1ae99b3c586224445
Sunny Aggrawal <sunnyagg@users.noreply.github.com>
2020-12-24 12:15:30 +0530

    Adds leaderelection package for push-consumer (#26)
    * leader election via consul registry

--- service/push-consumer/service.go
+++ service/push-consumer/service.go
@@ -2,8 +2,12 @@ package pushconsumer
 
 import (
 	"context"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/razorpay/metro/internal/health"
+	"github.com/razorpay/metro/pkg/leaderelection"
+	"github.com/razorpay/metro/pkg/logger"
 	"github.com/razorpay/metro/pkg/registry"
 )
 
@@ -13,6 +17,7 @@ type Service struct {
 	health   *health.Core
 	config   *Config
 	registry registry.Registry
+	NodeID   string
 }
 
 // NewService creates an instance of new push consumer service
@@ -20,6 +25,7 @@ func NewService(ctx context.Context, config *Config) *Service {
 	return &Service{
 		ctx:    ctx,
 		config: config,
+		NodeID: uuid.New().String(),
 	}
 }
 
@@ -28,16 +34,35 @@ func (c *Service) Start(errChan chan<- error) {
 	var err error
 
 	// Init the Registry
+	// TODO: move to component init ?
 	c.registry, err = registry.NewRegistry(&c.config.Registry)
 
 	if err != nil {
 		errChan <- err
 	}
 
-	//1. Register Node with Consul
+	// Add node to the registry
+	// TODO: use repo to add the node under /registry/nodes/{node_id} path
 
-	// 2. Create a channel and go routine
-	//    Go routine should try to do leader election, if elected as leader, push message to channel
+	// Run leader election
+	go leaderelection.RunOrDie(c.ctx, leaderelection.Config{
+		// TODO: read values from config
+		Name:          "metro-push-consumer",
+		Path:          "leader/election",
+		LeaseDuration: 30 * time.Second,
+		RenewDeadline: 20 * time.Second,
+		RetryPeriod:   5 * time.Second,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: func(ctx context.Context) {
+				c.lead(ctx)
+			},
+			OnStoppedLeading: func() {
+				c.stepDown()
+			},
+			OnNewLeader: func(identity string) {
+			},
+		},
+	}, c.registry)
 
 	// 3. Watch the Jobs/Node_id path for jobs
 
@@ -53,4 +78,12 @@ func (c *Service) Start(errChan chan<- error) {
 // Stop the service
 func (c *Service) Stop() error {
 	return nil
+}
+
+func (c *Service) lead(ctx context.Context) {
+	logger.Ctx(ctx).Infof("Node %s elected as new leader", c.NodeID)
+}
+
+func (c *Service) stepDown() {
+	logger.Log.Infof("Node %s stepping down from leader", c.NodeID)
 }

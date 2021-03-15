687863eba73ff3bfab59e92b7ff77e3448fa1cc2
shivasishdas <shivasish.das@razorpay.com>
2021-03-15 16:23:17 +0530

    adding a few recoverable subscriber errors

--- service/web/stream/error.go
+++ service/web/stream/error.go
@@ -1,12 +1,33 @@
 package stream
 
+import kafkapkg "github.com/confluentinc/confluent-kafka-go/kafka"
+
+var retryableKafkaErrorCodes map[kafkapkg.ErrorCode]bool
+
+func init() {
+	retryableKafkaErrorCodes = map[kafkapkg.ErrorCode]bool{
+		kafkapkg.ErrTimedOut:        true,
+		kafkapkg.ErrMsgTimedOut:     true,
+		kafkapkg.ErrRequestTimedOut: true,
+		// add more as identified in future
+	}
+}
+
 func isErrorRecoverable(err error) bool {
 
-	// TODO : implement!
-	switch err.(type) {
-	default:
+	if err == nil {
 		return true
 	}
 
+	switch err.(type) {
+	case kafkapkg.Error:
+		kErr := err.(kafkapkg.Error)
+		if kErr.IsRetriable() {
+			return true
+		}
+		return retryableKafkaErrorCodes[kErr.Code()]
+	default:
+		return false
+	}
 	return false
 }

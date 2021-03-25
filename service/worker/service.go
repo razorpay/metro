328ce4d3d64fd80e29989e356e23fdf74d7f98a4
Sunny Aggrawal <sunnyagg@outlook.com>
2021-03-25 18:00:44 +0530

    uses node binding id for subcriber id

--- service/worker/service.go
+++ service/worker/service.go
@@ -475,7 +475,7 @@ func (svc *Service) handleNodeBindingUpdates(ctx context.Context, newBindingPair
 
 		if !found {
 			logger.Ctx(ctx).Infow("binding added", "key", newBinding.Key())
-			handler := NewPushStream(ctx, newBinding.NodeID, newBinding.SubscriptionID, svc.subscriptionCore, svc.subscriber)
+			handler := NewPushStream(ctx, newBinding.ID, newBinding.SubscriptionID, svc.subscriptionCore, svc.subscriber)
 			svc.workgrp.Go(handler.Start)
 			svc.pushHandlers[newBinding.Key()] = handler
 		}

76be7549d02a9a2491b3ff1aa675c7fc61741c9e
Sunny Aggrawal <sunnyagg@outlook.com>
2021-03-26 15:30:25 +0530

    test fixes

--- internal/nodebinding/model_test.go
+++ internal/nodebinding/model_test.go
@@ -3,8 +3,8 @@ package nodebinding
 import (
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/razorpay/metro/internal/common"
-
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,11 +15,12 @@ func TestModel_Prefix(t *testing.T) {
 
 func TestModel_Key(t *testing.T) {
 	nodebinding := getDummyNodeBindingModel()
-	assert.Equal(t, nodebinding.Key(), nodebinding.Prefix()+nodebinding.NodeID+"/"+nodebinding.SubscriptionID)
+	assert.Equal(t, nodebinding.Key(), nodebinding.Prefix()+nodebinding.NodeID+"/"+nodebinding.SubscriptionID+"_"+nodebinding.ID[0:4])
 }
 
 func getDummyNodeBindingModel() *Model {
 	return &Model{
+		ID:             uuid.New().String(),
 		NodeID:         "testID",
 		SubscriptionID: "TestSubID",
 	}

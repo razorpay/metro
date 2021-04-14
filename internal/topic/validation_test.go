79fc1b149efa5ff688751d0c32fbe85590e00e28
shivasishdas <shivasish.das@razorpay.com>
2021-04-14 16:33:36 +0530

    adding validation ut

--- internal/topic/validation_test.go
+++ internal/topic/validation_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	metrov1 "github.com/razorpay/metro/rpc/proto/v1"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -25,4 +27,20 @@ func TestValidation_extractTopicMetaAndValidate(t *testing.T) {
 
 	proj, top, err = ExtractTopicMetaAndValidate(ctx, "projects/test-project/topics/to")
 	assert.NotNil(t, err)
+}
+
+func TestValidation_GetValidatedModel(t *testing.T) {
+	ctx := context.Background()
+
+	topic := "projects/test-project/topics/test-topic"
+	m, err := GetValidatedModel(ctx, &metrov1.Topic{
+		Name:   topic,
+		Labels: nil,
+	})
+	assert.Nil(t, err)
+
+	assert.Equal(t, m.Name, topic)
+	assert.Equal(t, m.RetryTopicName, topic+"-retry")
+	assert.Equal(t, m.DLQTopicName, topic+"-dlq")
+
 }

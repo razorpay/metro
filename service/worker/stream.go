b46fc16079dc24f3d8863f2df5c9fcaf78955f4d
shivasishdas <shivasish.das@razorpay.com>
2021-04-26 16:58:47 +0530

    Revert "move resp listen as a case"
    This reverts commit 221adc84b49674a296ff15d2ee5f80c36317ebe0.

--- service/worker/stream.go
+++ service/worker/stream.go
@@ -71,12 +71,13 @@ func (ps *PushStream) Start() error {
 			select {
 			case <-gctx.Done():
 				return gctx.Err()
-			case res := <-ps.subs.GetResponseChannel():
-				logger.Ctx(ps.ctx).Infow("worker: writing subscriber data to channel", "res", res)
-				ps.responseChan <- res
 			default:
-				logger.Ctx(ps.ctx).Infow("worker: sending a subscriber pull request")
+				logger.Ctx(ps.ctx).Debugw("worker: sending a subscriber pull request")
 				ps.subs.GetRequestChannel() <- &subscriber.PullRequest{10}
+				logger.Ctx(ps.ctx).Debugw("worker: waiting for subscriber data")
+				res := <-ps.subs.GetResponseChannel()
+				logger.Ctx(ps.ctx).Debugw("worker: writing subscriber data to channel", "res", res)
+				ps.responseChan <- res
 			}
 		}
 		logger.Ctx(ps.ctx).Infow("worker: returning from pull stream go routine")

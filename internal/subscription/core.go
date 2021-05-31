d66e6ee283a34c4f09022f6eba4827d29062e16e
shivasishdas <shivasish.das@razorpay.com>
2021-05-31 21:31:22 +0530

    nit revert

--- internal/subscription/core.go
+++ internal/subscription/core.go
@@ -110,7 +110,7 @@ func (c *Core) CreateSubscription(ctx context.Context, m *Model) error {
 	return c.repo.Create(ctx, m)
 }
 
-// Exists checks if subscription exists for a given Username
+// Exists checks if subscription exists for a given key
 func (c *Core) Exists(ctx context.Context, key string) (bool, error) {
 	subscriptionOperationCount.WithLabelValues(env, "Exists").Inc()
 
@@ -121,7 +121,7 @@ func (c *Core) Exists(ctx context.Context, key string) (bool, error) {
 
 	ok, err := c.repo.Exists(ctx, key)
 	if err != nil {
-		logger.Ctx(ctx).Errorw("error in executing exists", "msg", err.Error(), "Username", key)
+		logger.Ctx(ctx).Errorw("error in executing exists", "msg", err.Error(), "key", key)
 		return false, err
 	}
 	return ok, nil
@@ -222,7 +222,7 @@ func (c *Core) List(ctx context.Context, prefix string) ([]*Model, error) {
 	return out, nil
 }
 
-// Get returns subscription with the given Username
+// Get returns subscription with the given key
 func (c *Core) Get(ctx context.Context, key string) (*Model, error) {
 	subscriptionOperationCount.WithLabelValues(env, "Get").Inc()
 
@@ -237,7 +237,7 @@ func (c *Core) Get(ctx context.Context, key string) (*Model, error) {
 	}
 	prefix := common.GetBasePrefix() + Prefix + projectID + "/" + subscriptionName
 
-	logger.Ctx(ctx).Infow("fetching subscription", "Username", prefix)
+	logger.Ctx(ctx).Infow("fetching subscription", "key", prefix)
 
 	model := &Model{}
 	err = c.repo.Get(ctx, prefix, model)

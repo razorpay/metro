dfe8d0e10deeebe056fee40a49aeb8ec30073511
razorao <shashank.rao@razorpay.com>
2021-07-26 21:15:44 +0530

    Issue #114: Provide edit subscription and auto worker restart capability
    	--UTs for base model

--- internal/common/model_test.go
+++ internal/common/model_test.go
@@ -13,3 +13,16 @@ import (
 func TestBaseModel_Prefix(t *testing.T) {
 	assert.Equal(t, "metro-"+app.GetEnv()+"/", GetBasePrefix())
 }
+
+func TestSetVersionID(t *testing.T) {
+	m := BaseModel{}
+	m.SetVersionID("v1")
+	vid, _ := m.GetVersionID()
+	assert.Equal(t, "v1", vid)
+}
+
+func TestGetVersionIDUnset(t *testing.T) {
+	m := BaseModel{}
+	_, err := m.GetVersionID()
+	assert.NotNil(t, err)
+}

d8bbe830f0ee882b7a4fff971240faada01f904b
vnktram <venkataraman.r@razorpay.com>
2022-04-07 14:23:27 +0530

    more config changes

--- pkg/messagebroker/kafka.go
+++ pkg/messagebroker/kafka.go
@@ -114,14 +114,14 @@ func newKafkaProducerClient(ctx context.Context, bConfig *BrokerConfig, options 
 		"retries":                    3,
 		"linger.ms":                  0,
 		"request.timeout.ms":         3000,
-		"delivery.timeout.ms":        10000,
+		"delivery.timeout.ms":        2000,
 		"connections.max.idle.ms":    180000,
 		"log.queue":                  true,
 		"queue.buffering.max.kbytes": 65536, // Total message size sum allocated in buffer. Shared across topics/partitions
 		"go.logs.channel.enable":     true,  // Disable logs via channel
 		"go.events.channel.size":     10,    // Limit this to 1 to avoid outdated events
 		"go.produce.channel.size":    1000,  // Allocated buffer size for the produce channel.
-		"go.delivery.reports":        false, // Returns delivery acks
+		"go.delivery.reports":        true,  // Returns delivery acks
 		"go.batch.producer":          false, // Disable batch producer since it clubs calls to librdkafka across topics. This causes memory bloat.
 		"debug":                      "broker",
 	}

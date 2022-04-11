70895e5f1234f67e924a3a25ab7a8c1c0f425e89
vnktram <venkataraman.r@razorpay.com>
2022-04-11 14:13:07 +0530

    Revert kafka config changes

--- pkg/messagebroker/kafka.go
+++ pkg/messagebroker/kafka.go
@@ -109,21 +109,21 @@ func newKafkaProducerClient(ctx context.Context, bConfig *BrokerConfig, options 
 	logger.Ctx(ctx).Infow("kafka producer: initializing new", "options", options)
 
 	configMap := &kafkapkg.ConfigMap{
-		"bootstrap.servers":          strings.Join(bConfig.Brokers, ","),
-		"socket.keepalive.enable":    true,
-		"retries":                    3,
-		"linger.ms":                  0,
-		"request.timeout.ms":         3000,
-		"delivery.timeout.ms":        2000,
-		"connections.max.idle.ms":    180000,
-		"log.queue":                  true,
-		"queue.buffering.max.kbytes": 65536, // Total message size sum allocated in buffer. Shared across topics/partitions
-		"go.logs.channel.enable":     false, // Disable logs via channel
-		"go.events.channel.size":     10,    // Limit this to 1 to avoid outdated events
-		"go.produce.channel.size":    1000,  // Allocated buffer size for the produce channel.
-		"go.delivery.reports":        true,  // Returns delivery acks
-		"go.batch.producer":          false, // Disable batch producer since it clubs calls to librdkafka across topics. This causes memory bloat.
-		"debug":                      "broker",
+		"bootstrap.servers":       strings.Join(bConfig.Brokers, ","),
+		"socket.keepalive.enable": true,
+		"retries":                 3,
+		"linger.ms":               0,
+		"request.timeout.ms":      3000,
+		"delivery.timeout.ms":     2000,
+		"connections.max.idle.ms": 180000,
+		// "log.queue":                  true,
+		// "queue.buffering.max.kbytes": 65536, // Total message size sum allocated in buffer. Shared across topics/partitions
+		// "go.logs.channel.enable":     false, // Disable logs via channel
+		// "go.events.channel.size":     10,    // Limit this to 1 to avoid outdated events
+		// "go.produce.channel.size":    1000,  // Allocated buffer size for the produce channel.
+		// "go.delivery.reports":        true,  // Returns delivery acks
+		// "go.batch.producer":          false, // Disable batch producer since it clubs calls to librdkafka across topics. This causes memory bloat.
+		"debug": "broker",
 	}
 
 	if bConfig.EnableTLS {

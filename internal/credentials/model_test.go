0ace54b1fb1cdf7d5668dfdf1e2d83e8af5688d5
Akash Goyal <akash.goyal@razorpay.com>
2022-05-20 12:47:50 +0530

    added tested cases in model

--- internal/credentials/model_test.go
+++ internal/credentials/model_test.go
@@ -1,8 +1,10 @@
+//go:build unit
 // +build unit
 
 package credentials
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/razorpay/metro/internal/common"
@@ -44,4 +46,42 @@ func TestModel_HiddenPassword_Failure(t *testing.T) {
 	hiddenPwd, err := credentials.GetHiddenPassword()
 	assert.Empty(t, hiddenPwd)
 	assert.Equal(t, err, ErrPasswordNotInExpectedFormat)
+}
+
+func TestNewCredential(t *testing.T) {
+	type args struct {
+		username string
+		password string
+	}
+	password := "password"
+	pwd, _ := encryption.EncryptAsHexString([]byte(password))
+	credentials := &Model{
+		Username:  "project123__c525c7",
+		Password:  pwd,
+		ProjectID: "project123",
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Model
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				username: credentials.Username,
+				password: password,
+			},
+			want: credentials,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			println("-" + tt.args.password + "-")
+			println([]byte(tt.args.password))
+			println(encryption.EncryptAsHexString([]byte(password)))
+			if got := NewCredential(tt.args.username, tt.args.password); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewCredential() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }

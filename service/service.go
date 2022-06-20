33f6ac9d6e9799f97bf4b0f4582ada0f3818aaf7
Sakshi Goyal <sakshi.goyal@razorpay.com>
2022-06-20 16:12:42 +0530

    linting issue

--- service/service.go
+++ service/service.go
@@ -7,5 +7,6 @@ type IService interface {
 	// Start the service, it should be implemented as a blocking call
 	// method should return gracefully when ctx is Done
 	Start(ctx context.Context) error
+	// GetErrorChannel() will return the error channel for graceful service shutdown
 	GetErrorChannel() chan error
 }

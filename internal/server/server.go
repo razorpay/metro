8894a9e219cf65c5a789c4c7b858600fa3840f4e
Sakshi Goyal <sakshi.goyal@razorpay.com>
2022-07-19 19:10:33 +0530

    added route for commit.txt

--- internal/server/server.go
+++ internal/server/server.go
@@ -213,6 +213,8 @@ func newInternalServer() (*http.Server, error) {
 	mux.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
 	mux.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
 	mux.Handle("/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
+	mux.Handle("/commit.txt", http.FileServer(http.Dir("public/")))
+
 	server := http.Server{Handler: mux}
 
 	return &server, nil

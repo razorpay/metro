7c2ef871d0cf4990308557c5039522f221a1aa56
sumitkrzp <107676519+sumitkrzp@users.noreply.github.com>
2022-07-26 12:17:35 +0530

    unit-test for retrier start

--- internal/subscriber/retry/retrier_test.go
+++ internal/subscriber/retry/retrier_test.go
@@ -3,10 +3,22 @@
 package retry
 
 import (
-	"testing"
-
+	"context"
+	"github.com/golang/mock/gomock"
+	"github.com/golang/protobuf/proto"
+	"github.com/razorpay/metro/internal/brokerstore"
+	"github.com/razorpay/metro/internal/brokerstore/mocks"
+	"github.com/razorpay/metro/internal/subscription"
 	"github.com/razorpay/metro/internal/topic"
+	"github.com/razorpay/metro/pkg/cache"
+	mocks2 "github.com/razorpay/metro/pkg/cache/mocks"
+	"github.com/razorpay/metro/pkg/messagebroker"
+	mocks3 "github.com/razorpay/metro/pkg/messagebroker/mocks"
+	metrov1 "github.com/razorpay/metro/rpc/proto/v1"
 	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+	"time"
 )
 
 type intervalTest struct {
@@ -15,6 +27,13 @@ type intervalTest struct {
 	currentInterval                  int
 	want                             []float64
 }
+
+const (
+	delayTopic string = "projects/project123/topics/subscription-name.delay.5.seconds"
+	topicName  string = "primary-topic"
+	retryTopic string = "retry-topic"
+	partition  int32  = 0
+)
 
 func Test_Retrier(t *testing.T) {
 
@@ -86,4 +105,99 @@ func Test_Interval_Calculator(t *testing.T) {
 		actualIntervals := findAllRetryIntervals(test.min, test.max, test.currentRetryCount, test.maxRetryCount, test.currentInterval, availableDelayIntervals)
 		assert.Equal(t, test.want, actualIntervals)
 	}
+}
+
+func TestRetrier_Start(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockBrokerStore := mocks.NewMockIBrokerStore(ctrl)
+	mockCache := mocks2.NewMockICache(ctrl)
+	ctx := context.Background()
+	ctxWt, cancel := context.WithTimeout(ctx, time.Second*2)
+	defer cancel()
+	dcs := getMockDelayConsumerCore(ctx, ctrl)
+	type fields struct {
+		subscriberID   string
+		subs           *subscription.Model
+		bs             brokerstore.IBrokerStore
+		ch             cache.ICache
+		backoff        Backoff
+		finder         IntervalFinder
+		handler        MessageHandler
+		delayConsumers sync.Map
+		errChan        chan error
+	}
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "Start a Retrier",
+			fields: fields{
+				subscriberID:   "subscription-id",
+				subs:           getMockSubscriptionModel(),
+				bs:             mockBrokerStore,
+				ch:             mockCache,
+				backoff:        NewFixedWindowBackoff(),
+				finder:         NewClosestIntervalWithCeil(),
+				handler:        NewPushToPrimaryRetryTopicHandler(mockBrokerStore),
+				delayConsumers: sync.Map{},
+				errChan:        make(chan error),
+			},
+			args: args{ctxWt},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockBrokerStore.EXPECT().GetConsumer(
+				gomock.Any(),
+				gomock.Any(),
+			).Return(dcs, nil).AnyTimes()
+			r := &Retrier{
+				subscriberID:   tt.fields.subscriberID,
+				subs:           tt.fields.subs,
+				bs:             tt.fields.bs,
+				ch:             tt.fields.ch,
+				backoff:        tt.fields.backoff,
+				finder:         tt.fields.finder,
+				handler:        tt.fields.handler,
+				delayConsumers: tt.fields.delayConsumers,
+				errChan:        tt.fields.errChan,
+			}
+			err := r.Start(tt.args.ctx)
+			assert.Nil(t, err)
+		})
+	}
+}
+
+func getMockDelayConsumerCore(ctx context.Context, ctrl *gomock.Controller) *mocks3.MockConsumer {
+	dcs := mocks3.NewMockConsumer(ctrl)
+	req := messagebroker.GetTopicMetadataRequest{
+		Topic:     delayTopic,
+		Partition: partition,
+	}
+	dcs.EXPECT().GetTopicMetadata(ctx, req).Return(messagebroker.GetTopicMetadataResponse{}, nil).AnyTimes()
+	dcs.EXPECT().Pause(ctx, gomock.Any()).AnyTimes().Return(nil)
+	dcs.EXPECT().CommitByPartitionAndOffset(gomock.Any(), gomock.Any()).Return(
+		messagebroker.CommitOnTopicResponse{}, nil,
+	).AnyTimes()
+	dcs.EXPECT().Resume(gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
+	messages := make([]messagebroker.ReceivedMessage, 0, 3)
+	data, _ := proto.Marshal(&metrov1.PubsubMessage{Data: []byte("test")})
+	msgProto := messagebroker.ReceivedMessage{
+		Data:      data,
+		Topic:     topicName,
+		Partition: partition,
+	}
+	msgProto.MessageID = "1"
+	messages = append(messages, msgProto)
+	dcs.EXPECT().ReceiveMessages(gomock.Any(), gomock.Any()).Return(
+		&messagebroker.GetMessagesFromTopicResponse{
+			Messages: messages,
+		}, nil,
+	).AnyTimes()
+	return dcs
 }

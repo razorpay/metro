9166aaff6b2c60da28d961bfecbbf9bdda7cfe27
Sakshi Goyal <104060154+sakshi-goyal-razorpay@users.noreply.github.com>
2022-08-05 17:18:27 +0530

    Improve tracing for better debugging and monitoring (#394)
    * pass context in msg
    
    * fixing struct type
    
    * Eliminate zombie spans to avoid spamming otel collector
    
    Currently, this runaway span is causing most of the traffic that is generated by Metro to hypertrace(O11y). 
    This span does not correspond to any actual flow and is part of its own trace.
    Eliminating this to ease pressure on the hotel collector.
    
    * Fix missing ctx
    
    * add message received as an async span
    
    * removed message received span
    
    Co-authored-by: Venkat Ram <venkataraman.r@razorpay.com>

--- pkg/messagebroker/kafka.go
+++ pkg/messagebroker/kafka.go
@@ -474,26 +474,6 @@ func (k *KafkaBroker) ReceiveMessages(ctx context.Context, request GetMessagesFr
 			// extract the message headers and set in the response struct
 			receivedMessage := convertKafkaHeadersToResponse(msg.Headers)
 			receivedMessage.OrderingKey = string(msg.Key)
-
-			// Get span context from headers
-			carrier := kafkaHeadersCarrier(msg.Headers)
-			spanContext, extractErr := opentracing.GlobalTracer().Extract(opentracing.TextMap, &carrier)
-			if extractErr != nil {
-				logger.Ctx(ctx).Errorw("failed to get span context from message", "error", extractErr.Error())
-			}
-
-			messageSpan, _ := opentracing.StartSpanFromContext(
-				ctx,
-				"Kafka:MessageReceived",
-				opentracing.FollowsFrom(spanContext),
-				opentracing.Tags{
-					"message_id": receivedMessage.MessageID,
-					"topic":      msg.TopicPartition.Topic,
-					"partition":  msg.TopicPartition.Partition,
-					"offset":     msg.TopicPartition.Offset,
-				})
-			messageSpan.Finish()
-
 			receivedMessage.Data = msg.Value
 			receivedMessage.Topic = *msg.TopicPartition.Topic
 			receivedMessage.Partition = msg.TopicPartition.Partition

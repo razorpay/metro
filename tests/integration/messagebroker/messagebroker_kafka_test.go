bb583067e382058396b8d92b6c785d353faebb16
Sakshi Goyal <sakshi.goyal@razorpay.com>
2022-09-13 14:42:13 +0530

    fixing IT

--- tests/integration/messagebroker/messagebroker_kafka_test.go
+++ tests/integration/messagebroker/messagebroker_kafka_test.go
@@ -314,7 +314,7 @@ func Test_ResetAutoOffsetForConsumer(t *testing.T) {
 	aresp, err := admin.CreateTopic(context.Background(), messagebroker.CreateTopicRequest{
 		Name:          topic,
 		NumPartitions: 1,
-		Config: map[string]string{"retention.ms": 10000}
+		Config:        map[string]string{"retention.ms": "10000"},
 	})
 
 	assert.Nil(t, err)

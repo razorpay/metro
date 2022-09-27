e3a17c54b2a3eec6330c1f3a62efcc8cb12ac186
Akash Goyal <akash.goyal@razorpay.com>
2022-09-27 16:23:59 +0530

    added sub refresh cache

--- internal/tasks/schedulertask_test.go
+++ internal/tasks/schedulertask_test.go
@@ -9,19 +9,21 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
-
+	"github.com/razorpay/metro/internal/brokerstore"
 	mocks2 "github.com/razorpay/metro/internal/brokerstore/mocks"
 	"github.com/razorpay/metro/internal/node"
 	mocks3 "github.com/razorpay/metro/internal/node/mocks/core"
 	"github.com/razorpay/metro/internal/nodebinding"
 	mocks5 "github.com/razorpay/metro/internal/nodebinding/mocks/core"
+	"github.com/razorpay/metro/internal/scheduler"
 	mocks6 "github.com/razorpay/metro/internal/scheduler/mocks"
 	"github.com/razorpay/metro/internal/subscription"
 	mocks7 "github.com/razorpay/metro/internal/subscription/mocks/core"
 	"github.com/razorpay/metro/internal/topic"
 	mocks4 "github.com/razorpay/metro/internal/topic/mocks/core"
+	"github.com/razorpay/metro/pkg/registry"
 	"github.com/razorpay/metro/pkg/registry/mocks"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSchedulerTask_Run(t *testing.T) {
@@ -139,4 +141,132 @@ func TestSchedulerTask_Run(t *testing.T) {
 
 	cancel()
 	<-doneCh
+}
+
+func TestSchedulerTask_refreshCache(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	registryMock := mocks.NewMockIRegistry(ctrl)
+	// watcherMock := mocks.NewMockIWatcher(ctrl)
+	brokerstoreMock := mocks2.NewMockIBrokerStore(ctrl)
+	nodeCoreMock := mocks3.NewMockICore(ctrl)
+	topicCoreMock := mocks4.NewMockICore(ctrl)
+	subscriptionCoreMock := mocks7.NewMockICore(ctrl)
+	nodebindingCoreMock := mocks5.NewMockICore(ctrl)
+	schedulerMock := mocks6.NewMockIScheduler(ctrl)
+
+	workerID := uuid.New().String()
+	type fields struct {
+		id               string
+		registry         registry.IRegistry
+		brokerstore      brokerstore.IBrokerStore
+		nodeCore         node.ICore
+		scheduler        scheduler.IScheduler
+		topicCore        topic.ICore
+		nodeBindingCore  nodebinding.ICore
+		subscriptionCore subscription.ICore
+		nodeCache        map[string]*node.Model
+		subCache         map[string]*subscription.Model
+		topicCache       map[string]*topic.Model
+		nodeWatchData    chan *struct{}
+		subWatchData     chan *struct{}
+		topicWatchData   chan *struct{}
+	}
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Test1",
+			fields: fields{
+				id:               workerID,
+				registry:         registryMock,
+				brokerstore:      brokerstoreMock,
+				nodeCore:         nodeCoreMock,
+				scheduler:        schedulerMock,
+				topicCore:        topicCoreMock,
+				nodeBindingCore:  nodebindingCoreMock,
+				subscriptionCore: subscriptionCoreMock,
+				nodeCache:        make(map[string]*node.Model),
+				subCache:         make(map[string]*subscription.Model),
+				topicCache:       make(map[string]*topic.Model),
+				nodeWatchData:    make(chan *struct{}),
+				subWatchData:     make(chan *struct{}),
+				topicWatchData:   make(chan *struct{}),
+			},
+			args: args{
+				ctx: ctx,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sm := &SchedulerTask{
+				id:               tt.fields.id,
+				registry:         tt.fields.registry,
+				brokerstore:      tt.fields.brokerstore,
+				nodeCore:         tt.fields.nodeCore,
+				scheduler:        tt.fields.scheduler,
+				topicCore:        tt.fields.topicCore,
+				nodeBindingCore:  tt.fields.nodeBindingCore,
+				subscriptionCore: tt.fields.subscriptionCore,
+				nodeCache:        tt.fields.nodeCache,
+				subCache:         tt.fields.subCache,
+				topicCache:       tt.fields.topicCache,
+				nodeWatchData:    tt.fields.nodeWatchData,
+				subWatchData:     tt.fields.subWatchData,
+				topicWatchData:   tt.fields.topicWatchData,
+			}
+			dummyTopicModels := []*topic.Model{
+				{
+					Name:               "projects/test-project/topics/test",
+					NumPartitions:      2,
+					ExtractedTopicName: "test",
+					ExtractedProjectID: "test-project",
+					Labels:             map[string]string{},
+				},
+			}
+			// mock Topic Get
+			topicCoreMock.EXPECT().List(gomock.AssignableToTypeOf(ctx), "topics/").Return(
+				dummyTopicModels, nil).AnyTimes()
+			// mock nodes core
+			nodeCoreMock.EXPECT().List(gomock.AssignableToTypeOf(ctx), "nodes/").Return(
+				[]*node.Model{
+					{
+						ID: workerID,
+					},
+				}, nil).AnyTimes()
+			// mock subscription Core
+			sub := subscription.Model{
+				Name:                           "projects/test-project/subscriptions/test",
+				Topic:                          "projects/test-project/topics/test",
+				ExtractedTopicProjectID:        "test-project",
+				ExtractedSubscriptionName:      "test",
+				ExtractedSubscriptionProjectID: "test-project",
+				ExtractedTopicName:             "test",
+				DeadLetterPolicy: &subscription.DeadLetterPolicy{
+					DeadLetterTopic:     "projects/test-project/topics/test-dlq",
+					MaxDeliveryAttempts: 5,
+				},
+				PushConfig: &subscription.PushConfig{
+					PushEndpoint: "http://test.test",
+				},
+			}
+			sub.SetVersion("1")
+			subscriptionCoreMock.EXPECT().List(gomock.AssignableToTypeOf(ctx), "subscriptions/").Return(
+				[]*subscription.Model{&sub}, nil).AnyTimes()
+
+			if err := sm.refreshCache(tt.args.ctx); (err != nil) != tt.wantErr {
+				t.Errorf("SchedulerTask.refreshCache() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }

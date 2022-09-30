4f3a118bf75bbf7c1e92bc93c0f294a3ac023b15
Akash Goyal <akash.goyal@razorpay.com>
2022-09-30 18:24:02 +0530

    removed comments

--- service/web/service.go
+++ service/web/service.go
@@ -104,12 +104,10 @@ func (svc *Service) Start(ctx context.Context) error {
 
 	publisherCore := publisher.NewCore(brokerStore)
 
-	// Init Publisher task, this run the watchers on Registry
 	publisherTask, err := tasks.NewPublisherTask(uuid.New().String(), r, topicCore)
 	if err != nil {
 		return err
 	}
-	// Run the Publisher task
 	go func() {
 		runErr := publisherTask.Run(ctx)
 		if runErr != nil {

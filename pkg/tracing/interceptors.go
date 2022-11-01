73f76d4a6463188bbb050027508e464e6c0b90a4
Subin T P <subin.tp@razorpay.com>
2022-11-02 02:18:35 +0530

    Revert "Map error to grpc code"
    This reverts commit fc197499d003646697b0971ea6e7cf2889713f9b.

--- pkg/tracing/interceptors.go
+++ pkg/tracing/interceptors.go
@@ -102,19 +102,16 @@ func finishServerSpan(ctx context.Context, serverSpan opentracing.Span, err erro
 	}
 
 	if err != nil {
-
-		grpcError = err.ToGRPCError()
-
 		if s, ok := status.FromError(err); ok {
-			serverSpan.SetTag(GRPCStatusCode, grpcError.Code())
+			serverSpan.SetTag(GRPCStatusCode, s.Code())
 		}
 
 		ext.Error.Set(serverSpan, true)
 		serverSpan.LogFields(log.String("event", "error"), log.String("message", err.Error()))
 
 	} else {
-		serverSpan.SetTag(GRPCResponseCode, codes.OK)
 		serverSpan.SetTag(GRPCStatusCode, "0")
+		serverSpan.SetTag(GRPCResponseCode, codes.OK)
 		serverSpan.SetTag(GRPCResponseClass, Success)
 	}
 
